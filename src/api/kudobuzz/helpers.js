907a327d0e2d326d62e491b39b0624af566986ba
Selom <selom@kudobuzz.com>
2018-07-11 18:25:38 +0000

    moved promises to functinos

--- src/api/kudobuzz/helpers.js
+++ src/api/kudobuzz/helpers.js
@@ -1,18 +1,23 @@
 const Account = require('./accountModel')
 
 module.exports = _ => {
-    const promises = [
-        new Promise((resolve, reject) => {
-            Account.countDocuments({ 'payment_plan.plan': { $gt: 0 } }, (err, data) => {
-                resolve(data)
-            })
-        }),
-        new Promise((resolve, reject) => {
-            Account.countDocuments({}, (err, data) => {
-                resolve(data)
-            })
-        })
-    ]
+	const promises = [getTotalPayingUsers(), getTotalUsers()]
 
-    return Promise.all(promises)
+	return Promise.all(promises)
+}
+
+const getTotalPayingUsers = _ => {
+	return new Promise((resolve, reject) => {
+		Account.countDocuments({ 'payment_plan.plan': { $gt: 0 } }, (err, data) => {
+			resolve(data)
+		})
+	})
+}
+
+const getTotalUsers = _ => {
+	return new Promise((resolve, reject) => {
+		Account.countDocuments({}, (err, data) => {
+			resolve(data)
+		})
+	})
 }
